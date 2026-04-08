a20e55e18733844bbea8ade50e76ab1afe435eb5
박범수 <qjatn1102@naver.com>
2026-04-08 10:26:27 +0900

    기존 물리적 delete에서 softdelete로 수정완료 그로인해 del_post db에 컬럼 추가 0(있음) 1(삭제) 처리

--- src/main/java/com/example/demo/repositories/PostRepository.java
+++ src/main/java/com/example/demo/repositories/PostRepository.java
@@ -3,7 +3,6 @@ package com.example.demo.repositories;
 import com.example.demo.domain.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 
 public interface PostRepository extends JpaRepository <Post , Long>{
 }
